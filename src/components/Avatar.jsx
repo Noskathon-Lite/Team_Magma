8bf4417e68468d0b25d3dfefe7e3c9d6f467a82c
Babita00 <babitaasth07@gmail.com>
2025-01-12 17:19:40 +0545

    add Avatar component with facial expressions and animations

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,173 @@
+
+
+import { useAnimations, useFBX } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { button, useControls } from "leva";
+import React, { useEffect, useRef, useState } from "react";
+
+import * as THREE from "three";
+import { useChat } from "../hooks/useChat";
+
+
+import { useGraph } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
+import { SkeletonUtils } from 'three-stdlib'
+
+
+const facialExpressions = {
+    default: {},
+    smile: {
+        browInnerUp: 0.17,
+        eyeSquintLeft: 0.4,
+        eyeSquintRight: 0.44,
+        noseSneerLeft: 0.1700000727403593,
+        noseSneerRight: 0.14000002836874015,
+        mouthPressLeft: 0.61,
+        mouthPressRight: 0.41000000000000003,
+    },
+    funnyFace: {
+        jawLeft: 0.63,
+        mouthPucker: 0.53,
+        noseSneerLeft: 1,
+        noseSneerRight: 0.39,
+        mouthLeft: 1,
+        eyeLookUpLeft: 1,
+        eyeLookUpRight: 1,
+        cheekPuff: 0.9999924982764238,
+        mouthDimpleLeft: 0.414743888682652,
+        mouthRollLower: 0.32,
+        mouthSmileLeft: 0.35499733688813034,
+        mouthSmileRight: 0.35499733688813034,
+    },
+    sad: {
+        mouthFrownLeft: 1,
+        mouthFrownRight: 1,
+        mouthShrugLower: 0.78341,
+        browInnerUp: 0.452,
+        eyeSquintLeft: 0.72,
+        eyeSquintRight: 0.75,
+        eyeLookDownLeft: 0.5,
+        eyeLookDownRight: 0.5,
+        jawForward: 1,
+    },
+    surprised: {
+        eyeWideLeft: 0.5,
+        eyeWideRight: 0.5,
+        jawOpen: 0.351,
+        mouthFunnel: 1,
+        browInnerUp: 1,
+    },
+    angry: {
+        browDownLeft: 1,
+        browDownRight: 1,
+        eyeSquintLeft: 1,
+        eyeSquintRight: 1,
+        jawForward: 1,
+        jawLeft: 1,
+        mouthShrugLower: 1,
+        noseSneerLeft: 1,
+        noseSneerRight: 0.42,
+        eyeLookDownLeft: 0.16,
+        eyeLookDownRight: 0.16,
+        cheekSquintLeft: 1,
+        cheekSquintRight: 1,
+        mouthClose: 0.23,
+        mouthFunnel: 0.63,
+        mouthDimpleRight: 1,
+    },
+    crazy: {
+        browInnerUp: 0.9,
+        jawForward: 1,
+        noseSneerLeft: 0.5700000000000001,
+        noseSneerRight: 0.51,
+        eyeLookDownLeft: 0.39435766259644545,
+        eyeLookUpRight: 0.4039761421719682,
+        eyeLookInLeft: 0.9618479575523053,
+        eyeLookInRight: 0.9618479575523053,
+        jawOpen: 0.9618479575523053,
+        mouthDimpleLeft: 0.9618479575523053,
+        mouthDimpleRight: 0.9618479575523053,
+        mouthStretchLeft: 0.27893590769016857,
+        mouthStretchRight: 0.2885543872656917,
+        mouthSmileLeft: 0.5578718153803371,
+        mouthSmileRight: 0.38473918302092225,
+        tongueOut: 0.9618479575523053,
+    },
+};
+
+const corresponding = {
+    A: "viseme_PP",
+    B: "viseme_kk",
+    C: "viseme_I",
+    D: "viseme_AA",
+    E: "viseme_O",
+    F: "viseme_U",
+    G: "viseme_FF",
+    H: "viseme_TH",
+    X: "viseme_PP",
+};
+
+let setupMode = false;
+
+
+export function Avatar(props) {
+
+
+    const { scene } = useGLTF('/models/6780edf43b064b4dff0d2278.glb')
+    const clone = React.useMemo(() => SkeletonUtils.clone(scene), [scene])
+    const { nodes, materials } = useGraph(clone)
+
+
+    const { animations: IdleAnimation } = useFBX("/animations/Idle.fbx")
+    const { animations: AdministeringAnimation } = useFBX("/animations/Administering Cpr.fbx")
+    const { animations: DismissingAnimation } = useFBX("/animations/Dismissing Gesture.fbx")
+    const { animations: ThankfulAnimation } = useFBX("/animations/Thankful.fbx")
+    const { animations: LookAroundAnimation } = useFBX("/animations/Look Around.fbx")
+    const { animations: HeadShakeAnimation } = useFBX("/animations/Thoughtful Head Shake.fbx")
+
+    
+
+
+
+    IdleAnimation[0].name = "Idle";
+    AdministeringAnimation[0].name = "Administering";
+    DismissingAnimation[0].name = "Dismissing";
+    ThankfulAnimation[0].name = "Thankful";
+    HeadShakeAnimation[0].name = "HeadShake";  
+    LookAroundAnimation[0].name = "LookAround";
+
+
+    const [animation, setAnimation] = useState("LookAround")
+
+
+    const group = useRef();
+    const { actions } = useAnimations([IdleAnimation[0], AdministeringAnimation[0], DismissingAnimation[0], ThankfulAnimation[0], HeadShakeAnimation[0], LookAroundAnimation[0]], group)
+
+
+    useEffect(() => {
+        actions[animation].reset().fadeIn(0.5).play();
+        return () => actions[animation].fadeOut(0.5);
+    }, [animation])
+
+
+
+
+
+
+    return (
+        <group {...props} dispose={null} ref={group}>
+            <primitive object={nodes.Hips} />
+            <skinnedMesh geometry={nodes.Wolf3D_Outfit_Bottom.geometry} material={materials.Wolf3D_Outfit_Bottom} skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton} />
+            <skinnedMesh geometry={nodes.Wolf3D_Outfit_Footwear.geometry} material={materials.Wolf3D_Outfit_Footwear} skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton} />
+            <skinnedMesh geometry={nodes.Wolf3D_Outfit_Top.geometry} material={materials.Wolf3D_Outfit_Top} skeleton={nodes.Wolf3D_Outfit_Top.skeleton} />
+            <skinnedMesh name="EyeLeft" geometry={nodes.EyeLeft.geometry} material={materials.Wolf3D_Eye} skeleton={nodes.EyeLeft.skeleton} morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary} morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences} />
+            <skinnedMesh name="EyeRight" geometry={nodes.EyeRight.geometry} material={materials.Wolf3D_Eye} skeleton={nodes.EyeRight.skeleton} morphTargetDictionary={nodes.EyeRight.morphTargetDictionary} morphTargetInfluences={nodes.EyeRight.morphTargetInfluences} />
+            <skinnedMesh name="Wolf3D_Head" geometry={nodes.Wolf3D_Head.geometry} material={materials.Wolf3D_Skin} skeleton={nodes.Wolf3D_Head.skeleton} morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary} morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences} />
+            <skinnedMesh name="Wolf3D_Teeth" geometry={nodes.Wolf3D_Teeth.geometry} material={materials.Wolf3D_Teeth} skeleton={nodes.Wolf3D_Teeth.skeleton} morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary} morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences} />
+        </group>
+    )
+
+
+}
+
+useGLTF.preload('/models/6780edf43b064b4dff0d2278.glb')
