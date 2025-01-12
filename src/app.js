b585e4631a12c9c09c676917b239808e8535cd7e
Babita00 <babitaasth07@gmail.com>
2025-01-12 11:12:45 +0545

    add: configure CORS and set up user routes in the application

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import cors from "cors"
 import express from "express";
+import {userRoute} from "./routes/userRoutes"
 const app=express()
 
 app.use(express.json()); 
 
+app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    }),
+  );
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  
+  // Routes
+  app.use("/api/users", userRoute);
 
+  
 
 export {app}
