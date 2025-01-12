b56f45856f8b1c47e4a5fbe1e8963282b3d7a135
Babita00 <babitaasth07@gmail.com>
2025-01-12 11:16:06 +0545

    refactor: standardize user route definitions and improve import formatting

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,33 +1,33 @@
 import express from "express";
-import {changeCurrentPassword} from "../controllers/changePasswordController.js"
-import{forgotPassword} from "../controllers/forgotPasswordController.js"
-import {loginUser} from "../controllers/loginController.js"
+import { changeCurrentPassword } from "../controllers/changePasswordController.js";
+import { forgotPassword } from "../controllers/forgotPasswordController.js";
+import { loginUser } from "../controllers/loginController.js";
 import { logoutUser } from "../controllers/logoutController.js";
-import {registerUser} from "../controllers/signupController.js"
+import { registerUser } from "../controllers/signupController.js";
 import { updateUser } from "../controllers/updateProfile.js";
+import { verifyJWT } from "../middleware/jwtAuth.js";
 
-import { verifyJWT } from "../middleware/jwtAuth.js"; 
 const userRoute = express.Router();
 
 // User Registration
-router.post("/register", registerUser);
+userRoute.post("/register", registerUser);
 
 // User Login
-router.post("/login", loginUser);
+userRoute.post("/login", loginUser);
 
 // User Logout
-router.post("/logout", verifyJWT, logoutUser);
+userRoute.post("/logout", verifyJWT, logoutUser);
 
 // Change Current Password
-router.put("/change-password", verifyJWT, changeCurrentPassword);
+userRoute.put("/change-password", verifyJWT, changeCurrentPassword);
 
 // Forgot Password
-router.post("/forgot-password", forgotPassword);
+userRoute.post("/forgot-password", forgotPassword);
 
 // Reset Password
-router.post("/reset-password", resetPassword);
+userRoute.post("/reset-password", resetPassword);
 
 // Update User
-router.put("/update/:id", verifyJWT, updateUser);
+userRoute.put("/update/:id", verifyJWT, updateUser);
 
-export  {userRoute};
+export { userRoute };
