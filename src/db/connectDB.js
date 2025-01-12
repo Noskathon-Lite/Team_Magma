efaa51cac489c102ab65a505773721a9875802ab
Babita00 <babitaasth07@gmail.com>
2025-01-12 12:28:41 +0545

    fix: remove DB_Name constant from MongoDB connection string

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-import { DB_Name } from "../constants.js";
+// import { DB_Name } from "../constants.js";
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${DB_Name}`
+      `${process.env.MONGODB_URL}`
     );
+    
     console.log(
         `MongoDB connected !! Host:${connectionInstance.connection.host}`
       );
