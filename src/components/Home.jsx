9ca34cb3bd2ec93e6f7f0f203cecc8fd2faf940f
Babita00 <babitaasth07@gmail.com>
2025-01-13 07:39:10 +0545

    feat: add Home component with 3D canvas and UI integration

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import { Canvas } from "@react-three/fiber";
+import { Loader } from "@react-three/drei";
+import { Leva } from "leva";
+import { Experience } from "./Experience";
+import { UI } from "./UI";
+
+const Home = () => {
+  return (
+    <>
+      <Loader />
+      <Leva hidden />
+      <UI />
+      <Canvas shadows camera={{ position: [0, 0, 1], fov: 30 }}>
+        <Experience />
+      </Canvas>
+    </>
+  );
+};
+
+export default Home;
